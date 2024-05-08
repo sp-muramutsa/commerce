97c6dd236b760a45937d831a230c89423978b311
sp-muramutsa <125483553+sp-muramutsa@users.noreply.github.com>
2024-05-08 10:39:43 -0500

    Removed unnecessary models: Address and Order

--- auctions/models.py
+++ auctions/models.py
@@ -5,13 +5,14 @@ from django.core.exceptions import ValidationError
 from django.core.validators import MinValueValidator
 from django.db import models
 
+
 class User(AbstractUser):
     date_of_birth = models.DateField(null=True)
-    address = models.ForeignKey("Address", on_delete=models.PROTECT, null=True)
     phone_number = models.CharField(max_length=15, null=True)
 
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
+
 
 class Listing(models.Model):
     title = models.CharField(max_length=100)
@@ -23,41 +24,11 @@ class Listing(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
     bids = models.ManyToManyField("Bid", related_name="listing_bids")
-    orders = models.ManyToManyField("Order", related_name="listing_orders")
     winning_bid = models.ForeignKey("Bid", on_delete=models.SET_NULL, null=True, blank=True, related_name="winning_bid_listing")
     watchlist = models.ManyToManyField("Watchlist", related_name="watchlisted_by", blank=True)
 
     def __str__(self):
          return f"Title: {self.title}\nCategory: {self.category}\nSeller: {self.seller.first_name} {self.seller.last_name}\nStatus: {'Active' if self.is_active else 'Sold to ' + str(self.winning_bid.bidder) + ' at a price of ' + str(self.winning_bid.amount) +'$'}\n\n"
-
-
-
-class Bid(models.Model):
-    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids_listing")
-    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.amount} bid by {self.bidder}"
-
-class Order(models.Model):
-    buyer = models.ForeignKey(User, on_delete=models.CASCADE)
-    listings = models.ManyToManyField(Listing, related_name="order_listings")
-    quantity = models.PositiveIntegerField(default=1)
-    total_price = models.DecimalField(max_digits=10, decimal_places=2)
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.quantity} units, bought by {self.buyer}, for a total of {self.total_price}"
-
-class Comment(models.Model):
-    comment = models.TextField()
-    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenters")
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.comment}"
 
 
 class Watchlist(models.Model):   
@@ -68,11 +39,20 @@ class Watchlist(models.Model):
        return f"{self.user}'s watchlisted {self.item}"
 
 
-class Address(models.Model):
-    country = models.CharField(max_length=100, null=False)
-    street_address = models.CharField(max_length=255, null=False)
-    city = models.CharField(max_length=100)
-    zip_code = models.CharField(max_length=20)
+class Bid(models.Model):
+    listing = models.ForeignKey(Listing, on_delete=models.CASCADE, related_name="bids_listing")
+    bidder = models.ForeignKey(User, on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.street_address}, {self.city}, {self.country} - {self.zip_code}"
+        return f"{self.amount} bid by {self.bidder}"
+    
+
+class Comment(models.Model):
+    comment = models.TextField()
+    commenter = models.ForeignKey(User, on_delete=models.CASCADE, related_name="commenters")
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.comment}"
